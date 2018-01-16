ea801242b9d6b4d22b9807d5aae7d36165e4409e
Ivan Limongan <ivan.limongan@gmail.com>
2018-01-16 09:51:02 -0800

    pulling api content update, more efficient

--- src/components/articles/Article.js
+++ src/components/articles/Article.js
@@ -5,49 +5,32 @@ import Template from '../Template';
 const template = new Template(html);
 
 export default class Article {
-  constructor(article) {
-    // Get "source" from api
-    this.source = article.source; 
-    this.author = article.author;
-    this.title = article.title;
-    this.publishedAt = article.publishedAt;
-    this.url = article.url;
-    this.description = article.description;
-    this.image = article.urlToImage;
-  
+  constructor(articles) {
+    // Get from api
+    this.articles = articles;
   }
 
   render() {
     const dom = template.render();
 
     // Set path from api
-    const source = this.source;
-    const author = this.author;
-    const title = this.title;
-    const publishedAt = this.publishedAt;
-    const url = this.url;
-    const description = this.description;
-    const image = this.image;
+    const article = this.articles;
 
     // Add content from api
-    dom.querySelector('.title').textContent = title;
-    dom.querySelector('.author').textContent = author;
-    dom.querySelector('.publisher').textContent = source.name;
-    dom.querySelector('.publishedAt').textContent = publishedAt;
-    // dom.querySelector('.url').textContent = url;
-    dom.querySelector('.description').textContent = description;
-
-    // console.log(articles[0].author);
-    // console.log(source.title);
-    // console.log(source.name);
+    dom.querySelector('.title').textContent = article.title;
+    dom.querySelector('.author').textContent = article.author;
+    dom.querySelector('.publisher').textContent = article.source.name;
+    dom.querySelector('.publishedAt').textContent = article.publishedAt;
+    // dom.querySelector('.url').textContent = article.url;
+    dom.querySelector('.description').textContent = article.description;
 
     // Add src and alt to images
     const img = dom.querySelector('.newsImage');
-    if(image) {
-      img.setAttribute('src', image);
-      img.setAttribute('alt', `${title} by ${author}`);
+    if(article.urlToImage) {
+      img.setAttribute('src', article.urlToImage);
+      img.setAttribute('alt', `${article.title} by ${article.author}`);
     }
-    else {
+    else{
       img.classList.add('hidden');
     }
 
